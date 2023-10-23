6b81c8db577a17b2584e324aabe69b06e992c316
Jadstar <jaden.kostogiannis@gmail.com>
2023-10-24 01:55:57 +1100

    fetch robot working
    trying to get collision detection when moving but not workign yet

--- fetch_robotv2.py
+++ fetch_robotv2.py
@@ -1,39 +1,55 @@
 #!/usr/bin/env python
-
+import swift
 import numpy as np
 from roboticstoolbox.robot.Robot import Robot
+import roboticstoolbox as rtb
 
 # from spatialmath import SE3
 
 
 class Fetch(Robot):
     """
-    Class that imports a Fetch URDF model
+    Heavily taken from Robotics Toolbox for Python
+    https://petercorke.github.io/robotics-toolbox-python/arm_erobot.html#defined-from-urdf
 
-    ``Fetch()`` is a class which imports a Fetch robot definition
-    from a URDF file.  The model describes its kinematic and graphical
-    characteristics.
+    As could not figure out how to define DH params, use ETS to define robot
+    plus  URDF allows for ROS integration 
 
-    .. runblock:: pycon
+    ERobot: fetch (by Fetch), 10 joints (RPPRRRRRRR), 1 gripper, 6 branches, dynamics, geometry, collision
+    ┌─────┬────────────────────┬───────┬────────────────────┬─────────────────────────────────────────────────────────┐
+    │link │        link        │ joint │       parent       │                   ETS: parent to link                   │
+    ├─────┼────────────────────┼───────┼────────────────────┼─────────────────────────────────────────────────────────┤
+    │   0 │ base0              │       │ BASE               │ SE3()                                                   │
+    │   1 │ base1              │     0 │ base0              │ SE3() ⊕ Rz(q0)                                          │
+    │   2 │ base_link          │     1 │ base1              │ SE3() ⊕ tx(q1)                                          │
+    │   3 │ torso_lift_link    │     2 │ base_link          │ SE3(-0.08687, 0, 0.3774; -3.508e-15°, -0°, 0°) ⊕ tz(q2) │
+    │   4 │ shoulder_pan_link  │     3 │ torso_lift_link    │ SE3(0.1195, 0, 0.3486) ⊕ Rz(q3)                         │
+    │   5 │ shoulder_lift_link │     4 │ shoulder_pan_link  │ SE3(0.117, 0, 0.06) ⊕ Ry(q4)                            │
+    │   6 │ upperarm_roll_link │     5 │ shoulder_lift_link │ SE3(0.219, 0, 0) ⊕ Rx(q5)                               │
+    │   7 │ elbow_flex_link    │     6 │ upperarm_roll_link │ SE3(0.133, 0, 0) ⊕ Ry(q6)                               │
+    │   8 │ forearm_roll_link  │     7 │ elbow_flex_link    │ SE3(0.197, 0, 0) ⊕ Rx(q7)                               │
+    │   9 │ wrist_flex_link    │     8 │ forearm_roll_link  │ SE3(0.1245, 0, 0) ⊕ Ry(q8)                              │
+    │  10 │ @wrist_roll_link   │     9 │ wrist_flex_link    │ SE3(0.1385, 0, 0) ⊕ Rx(q9)                              │
+    │  11 │ bellows_link       │       │ torso_lift_link    │ SE3()                                                   │
+    │  12 │ bellows_link2      │       │ torso_lift_link    │ SE3()                                                   │
+    │  13 │ estop_link         │       │ base_link          │ SE3(-0.1246, 0.2389, 0.3113; 90°, -0°, 0°)              │
+    │  14 │ laser_link         │       │ base_link          │ SE3(0.235, 0, 0.2878; -180°, -0°, 0°)                   │
+    │  15 │ torso_fixed_link   │       │ base_link          │ SE3(-0.08687, 0, 0.3774; -3.508e-15°, -0°, 0°)          │
+    └─────┴────────────────────┴───────┴────────────────────┴─────────────────────────────────────────────────────────┘
 
-        >>> import roboticstoolbox as rtb
-        >>> robot = rtb.models.URDF.Fetch()
-        >>> print(robot)
+    qr = arm aligned in x axis
+    qz = arm tucked in 
+    ┌─────┬─────┬────┬───────┬────────┬────────┬────────┬────────┬─────┬────────┬─────┐
+    │name │ q0  │ q1 │ q2    │ q3     │ q4     │ q5     │ q6     │ q7  │ q8     │ q9  │
+    ├─────┼─────┼────┼───────┼────────┼────────┼────────┼────────┼─────┼────────┼─────┤
+    │  qr │  0° │  0 │  0.05 │  75.6° │  80.2° │ -11.5° │  98.5° │  0° │  95.1° │  0° │
+    │  qz │  0° │  0 │  0    │  0°    │  0°    │  0°    │  0°    │  0° │  0°    │  0° │
 
-    Defined joint configurations are:
-
-    - qz, zero joint angle configuration, arm is stretched out in the x-direction
-    - qr, tucked arm configuration
-
-    .. codeauthor:: Kerry He
-    .. sectionauthor:: Peter Corke
     """
 
     def __init__(self):
 
-        links, name, urdf_string, urdf_filepath = self.URDF_read(
-            "fetch_description/robots/fetch.urdf"
-        )
+        links, name, urdf_string, urdf_filepath = self.URDF_read(tld="",file_path="fetch_robotdata/robots/fetch.urdf")
 
         super().__init__(
             links,
@@ -55,9 +71,12 @@ class Fetch(Robot):
 
 if __name__ == "__main__":  # pragma nocover
 
-    robot = Fetch()
-    print(robot)
+    env = swift.Swift()
+    env.launch(realtime= True)
 
+    robot = Fetch()
+    print(robot.q)
+    env.add(robot)
     for link in robot.links:
         print(link.name)
         print(link.isjoint)
@@ -69,3 +88,19 @@ if __name__ == "__main__":  # pragma nocover
         print(link.name)
         print(link.isjoint)
         print(len(link.collision))
+
+    # robot.plot(robot.q)
+    # print(robot.qz)
+    # tr = robot.fkine(robot.qr).A
+    # print(tr)
+    # finalpos = robot.ikine_LM(robot.fkine(robot.qr).A,q0=robot.qr,joint_limits=True)
+
+    # if finalpos.success:
+    #     qgoal = finalpos.q
+    #     qtraj = rtb.jtraj(robot.q,qgoal,50,).q
+
+    # for q in qtraj:
+    #     robot.q = q
+    #     env.step(0.02)
+
+    env.hold()
