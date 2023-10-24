63e10dc027aaa45fbd9fdb47f5a754e6b832ff7f
Jared Mayo <jared.mayo@student.uts.edu.au>
2023-10-24 17:50:50 +1100

    color changing patty

--- cooking.py
+++ cooking.py
@@ -7,7 +7,9 @@ import roboticstoolbox as rtb
 from fetch_robot import Fetch
 from FetchCam import FetchCamera
 from spatialmath import SE3
-
+import os
+import spatialgeometry as geometry
+import numpy as np
 
 ROOM_TEMP = 20  # in Celsius
 FLIP_TEMP = 70  # temperature to flip the patty
@@ -19,7 +21,77 @@ patty_position = (0,1,0)
 above_patty = (0,1,.5)
 plate_position = (0,2,0)
 above_plate = (0,2,0.5)
+class Patty:
+    def __init__(self, position, env):
+        '''
+        Initializes the Patty in the simulation environment.
+        
+        position: A tuple (x, y, z) indicating where the patty should be placed.
+        env: The simulation environment.
+        '''
+        self.position = position
+        self.env = env
 
+        # Initialize patty temperature to room temperature
+        self.temperature = ROOM_TEMP
+        
+        # Determine color based on temperature
+        self.is_flipped = False
+        self.color = self.get_color(self.temperature)
+        # Load the patty into the simulation at the given position
+        self.mesh = geometry.Mesh('assets/BurgerPatty.stl', pose=SE3(position), color=self.color)
+        self.env.add(self.mesh)
+        self.update_color()
+
+    def heat(self, delta_temp):
+        '''
+        Increase the temperature of the patty and update its color accordingly.
+
+        delta_temp: Amount of temperature increase.
+        '''
+        self.temperature += delta_temp
+        self.update_color()
+
+    def get_color(self, temp):
+        '''
+        Determines the color of the patty based on its temperature.
+        
+        temp: The current temperature of the patty.
+        
+        Returns: A tuple representing the RGB color.
+        '''
+        # Use linear interpolation to transition from pink (uncooked) to brown (cooked)
+        # based on the temperature relative to the flipping and done temperatures.
+        if temp < FLIP_TEMP:
+            fraction_cooked = (temp - ROOM_TEMP) / (FLIP_TEMP - ROOM_TEMP)
+        else:
+            fraction_cooked = 1 + (temp - FLIP_TEMP) / (DONE_TEMP - FLIP_TEMP)
+        
+        # Pink color
+        pink = np.array([255, 182, 193])
+        # Brown color
+        brown = np.array([139, 69, 19])
+
+        # Interpolate between pink and brown
+        color = pink + fraction_cooked * (brown - pink)
+        clamped_color = np.clip(color, 0, 255)
+
+        # Return as a tuple normalized to [0, 1]
+        return tuple(clamped_color / 255.0)
+
+    def update_color(self):
+        '''
+        Updates the color of the patty mesh in the environment based on its temperature.
+        '''
+        # Calculate the new color based on temperature
+        new_color = self.get_color(self.temperature)
+        self.mesh.color = new_color
+        
+        # Re-add the patty to the environment to force a visual update
+        self.env.remove(self.mesh)
+        self.mesh = geometry.Mesh('assets/BurgerPatty.stl', pose=SE3(self.position), scale=[0.005, 0.005, 0.005], color=new_color)
+        self.env.add(self.mesh)
+        
 
 class CookingRobot:
     '''
@@ -37,9 +109,11 @@ class CookingRobot:
         self.camera = FetchCamera()
         self.spatula_mount = ''
         self.spatula = ''
+        patty_position = (0, 0, 0)
+        self.patty = Patty(patty_position, self.env)  # Provide the position and environment when creating the Patty
+
 
         # Initialization
-        self.patty_temp = ROOM_TEMP
         self.patty_flipped = False
         self.patty_is_cooked = False
 
@@ -61,8 +135,6 @@ class CookingRobot:
         '''
 
 
-
-
     def flip_patty(self, location):
         # 1. Move gripper above the patty
         q_above_patty = self.robot.ik(above_patty)  # This should be a position above the patty
@@ -71,7 +143,6 @@ class CookingRobot:
         # 2. Descend to grip the patty
         q_grip_patty = self.robot.ik(patty_position)  # This should be a position to grip the patty
         self.robot.q = q_grip_patty
-        self.robot.gripper.close()  # Assuming a gripper close function
         
         # 3. Lift it slightly
         self.robot.q = q_above_patty
@@ -83,7 +154,6 @@ class CookingRobot:
         
         # 5. Lower the patty back
         self.robot.q = q_grip_patty
-        self.robot.gripper.open()  # Release the patty
 
         # 6. Return to initial pose or suitable rest pose
         self.robot.q = q_above_patty
@@ -113,15 +183,15 @@ class CookingRobot:
     def cook(self):
         while not self.patty_is_cooked:
             # Simulate cooking
-            self.patty_temp += 0.5  # This is a simple linear simulation for illustration
+            self.patty.heat(0.5)  # This updates the patty's temperature and color
 
             # Check if it's time to flip the patty
-            if self.patty_temp >= FLIP_TEMP and not self.patty_flipped:
+            if self.patty.temperature >= FLIP_TEMP and not self.patty_flipped:
                 self.flip_patty()
                 self.patty_flipped = True
 
             # Check if patty is cooked
-            if self.patty_flipped and self.patty_temp >= DONE_TEMP:
+            if self.patty_flipped and self.patty.temperature >= DONE_TEMP:
                 self.move_to_plate()
                 self.patty_is_cooked = True
 
--- main.py
+++ main.py
@@ -5,9 +5,16 @@
  
 '''
 import swift
+import time
 import roboticstoolbox as rtb
 from cooking import CookingRobot, FLIP_TEMP, DONE_TEMP, TIME_STEP
 
+def testpatty(robot):
+    # Test heating the patty and visualizing the color change
+    while robot.patty.temperature < DONE_TEMP:
+        robot.patty.heat(1)  # Increase temperature by 1 degree for testing
+        print(f"Current Patty Temperature: {robot.patty.temperature}°C")  # Print the current temperature
+        time.sleep(0.2)
 def main():
     # Initialize the simulation environment
     env = swift.Swift()
@@ -15,6 +22,9 @@ def main():
 
     # Load the robot and cooking items
     robot = CookingRobot(env)
+    
+    # Test the patty color change
+    testpatty(robot)
 
     # Start the cooking process
     robot.cook()
