6753a9ecb67765a0ae4e660a920db1bda0c822eb
Jared Mayo <jared.mayo@student.uts.edu.au>
2023-10-28 18:00:59 +1100

    start stop

--- GUI.py
+++ GUI.py
@@ -1,5 +1,7 @@
 # GUI.py
 from PyQt5.QtWidgets import QWidget, QGraphicsView, QGraphicsScene, QGraphicsEllipseItem, QLabel
+from PyQt5.QtWidgets import QVBoxLayout, QLabel, QSlider, QPushButton, QSpinBox, QHBoxLayout
+
 from PyQt5.QtGui import QBrush, QColor
 from PyQt5.QtCore import Qt
 from cooking import FLIP_TEMP, DONE_TEMP
@@ -28,6 +30,34 @@ class PattyVisualizer(QWidget):
         # Temperature label
         self.label = QLabel(self)
         self.label.setGeometry(100, 350, 200, 40)
+
+        # Emergency Stop Button
+        self.emergency_stop_btn = QPushButton("Emergency Stop", self)
+        self.emergency_stop_btn.setGeometry(125, 380, 150, 40)
+        self.emergency_stop_requested = False  # Flag to indicate if emergency stop was requested
+        self.emergency_stop_btn.clicked.connect(self.handle_emergency_stop)
+
+    def handle_emergency_stop(self):
+        if not self.emergency_stop_requested:  # If currently in the "Emergency Stop" state
+            self.emergency_stop_requested = True
+            self.emergency_stop_btn.setStyleSheet("background-color: green")
+            self.emergency_stop_btn.setText("START")
+        else:  # If currently in the "START" state
+            self.show_confirmation_dialog()
+
+    def show_confirmation_dialog(self):
+        from PyQt5.QtWidgets import QMessageBox
+        msg_box = QMessageBox(self)
+        msg_box.setWindowTitle("Confirmation")
+        msg_box.setText("Are you sure the area is safe to start?")
+        msg_box.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+        msg_box.setDefaultButton(QMessageBox.No)
+        reply = msg_box.exec_()
+        if reply == QMessageBox.Yes:
+            self.emergency_stop_requested = False
+            self.emergency_stop_btn.setStyleSheet("background-color: red")
+            self.emergency_stop_btn.setText("Emergency Stop")
+            # Additional logic to start the process can be added here
 
     def update_display(self, temperature):
         self.label.setText(f"Current Patty Temperature: {temperature}°C")
--- main.py
+++ main.py
@@ -5,14 +5,25 @@ from GUI import PattyVisualizer
 from PyQt5.QtWidgets import QApplication
 from spatialmath import SE3
 import threading
+
 def testpatty(patty, window, env):
     
-    
-    while patty.temperature < DONE_TEMP:
-        patty.heat(1)  # Increase temperature by 1 degree for testing
-        print(f"Current Patty Temperature: {patty.temperature}°C")
-        window.update_display(patty.temperature)
-        time.sleep(0.2)
+    while True:
+        # If emergency stop is not requested, continue cooking
+        while not window.emergency_stop_requested and patty.temperature < DONE_TEMP:
+            patty.heat(1)  # Increase temperature by 1 degree for testing
+            print(f"Current Patty Temperature: {patty.temperature}°C")
+            window.update_display(patty.temperature)
+            time.sleep(0.2)
+
+        # If the patty is done cooking, break out of the loop
+        if patty.temperature >= DONE_TEMP:
+            break
+
+        # If emergency stop is requested, pause and wait for it to be unset
+        while window.emergency_stop_requested:
+            time.sleep(0.5)
+
 
 def main():
     # Initialize the simulation environment
@@ -21,8 +32,10 @@ def main():
     patty = Patty(env=env)
     patty.setPose(SE3(2, 1, 1))
     patty.AddtoEnv(env)
+    
     # Load the robot and cooking items
     robot = CookingRobot()
+    robot.AddtoEnv(env=env)
     
     # Initialize the GUI for Patty Visualization
     app = QApplication([])
@@ -32,10 +45,6 @@ def main():
     # Start the testpatty function in a separate thread
     test_thread = threading.Thread(target=testpatty, args=(patty, window, env))
     test_thread.start()
-
-
-    # Start the cooking process
-    # robot.cook(patty)
 
     # Start the GUI's main loop
     app.exec_()
